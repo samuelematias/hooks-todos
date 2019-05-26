60835458f00dd62fac1dd8c55894a94d92fa7b9e
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-26 20:47:32 -0300

    init the project and Avoiding Props Drilling with React Context and the useContext Hook

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+import { UserContext } from './index';
+
+export default function App() {
+	const value = useContext(UserContext);
+	return (
+		<div>
+			<div>{`Hello, ${value}`}</div>
+		</div>
+	);
+}
