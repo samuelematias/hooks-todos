e25aa0efeca2ffb737d56ba24ac336756589d669
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-27 11:16:33 -0300

    Combining useContext and useReducer to Make Initial App State

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodosContext from './context';
+import todosReducer from './reducer';
 
-export const UserContext = React.createContext();
+import TodoList from './components/TodosList';
 
-const username = 'Samuel';
+const App = () => {
+	const initState = useContext(TodosContext);
+	const [state, dispatch] = useReducer(todosReducer, initState);
 
-ReactDOM.render(
-	<UserContext.Provider value={username}>
-		<App />
-	</UserContext.Provider>,
-	document.getElementById('root')
-);
+	return (
+		<TodosContext.Provider value={{ state, dispatch }}>
+			<TodoList />
+		</TodosContext.Provider>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
