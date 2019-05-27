bb8a07d33bff3f0af6f01acf7827c0808d7e5966
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-27 14:26:39 -0300

    Delete Request to Remove Todos

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import TodosContext from '../context';
+import axios from 'axios';
 
 export default function TodoList() {
 	const { state, dispatch } = useContext(TodosContext);
@@ -35,7 +36,12 @@ export default function TodoList() {
 							/>
 						</button>
 						<button
-							onClick={() => dispatch({ type: 'REMOVE_TODO', payload: todo })}
+							onClick={async () => {
+								await axios.delete(
+									`https://hooks-todos-api-4l6l4i9an.now.sh/todos/${todo.id}`
+								);
+								dispatch({ type: 'REMOVE_TODO', payload: todo });
+							}}
 						>
 							<img
 								src="https://icon.now.sh/delete/8b0000"
