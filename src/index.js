3e5751b2196fff3831aebea381c9ec36afa76957
Samuel Mataraso <samuelmataraso@Samuels-MacBook-Pro.local>
2019-05-27 14:22:06 -0300

    Creating a Custom Hook to Fetch Initial App Data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useState, useEffect, useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import TodosContext from './context';
 import todosReducer from './reducer';
+import axios from 'axios';
 
 import TodoList from './components/TodosList';
 import TodoForm from './components/TodoForm';
 
+const useApi = endpoint => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const getData = async () => {
+		const response = await axios.get(endpoint);
+		setData(response.data);
+	};
+
+	return data;
+};
+
 const App = () => {
 	const initState = useContext(TodosContext);
 	const [state, dispatch] = useReducer(todosReducer, initState);
+	const savedTodos = useApi('https://hooks-todos-api-4l6l4i9an.now.sh/todos');
+
+	useEffect(() => {
+		dispatch({
+			type: 'GET_TODOS',
+			payload: savedTodos
+		});
+	}, [savedTodos]);
 
 	return (
 		<TodosContext.Provider value={{ state, dispatch }}>
